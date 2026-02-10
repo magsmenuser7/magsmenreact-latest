cefc7a53394b09d9d1438b0234bb3838aa5c8bae
magsmenuser7 <magsmenuser7@gmail.com>
2026-02-10 10:57:26 +0530

    chages done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -246,7 +246,7 @@ const Header = () => {
               ))}
 
               <a
-                href="https://schoolofstrategy.magsmen.com/"
+                href="/final-brand-presentation-for-printing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="mt-8 bg-white text-[#683FBF] px-6 py-3 rounded-lg font-semibold text-center hover:bg-[#5a35a3] hover:text-white transition-colors shadow-lg w-full"
